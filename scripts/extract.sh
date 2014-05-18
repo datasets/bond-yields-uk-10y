9e9212839c8eda0a242c2cd9de544e0e50330459
Rufus Pollock <rufus.pollock@okfn.org>
2014-05-18 19:45:01 +0100

    [refactor][s]: move data to data directory.

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Year,Rate" > annual.csv
-cat cache/10y.csv | tail -n+2 | sed "s/^.\{7\}\(....\)/\1-12-31/" | sed "s///g" >> annual.csv
+echo "Year,Rate" > data/annual.csv
+cat cache/10y.csv | tail -n+2 | sed "s/^.\{7\}\(....\)/\1-12-31/" | sed "s///g" >> data/annual.csv
 
 # quarterly
-echo "Date,Rate" > quarterly.csv
-cat cache/10y-q.csv | tail -n+2 | sed "s/^\(..\) \(...\) \(....\)/\3-\2-\1/" | sed "s/Mar/03/" | sed "s/Jun/06/" | sed "s/Sep/09/" | sed "s/Dec/12/" | sed "s///g" >> quarterly.csv 
+echo "Date,Rate" > data/quarterly.csv
+cat cache/10y-q.csv | tail -n+2 | sed "s/^\(..\) \(...\) \(....\)/\3-\2-\1/" | sed "s/Mar/03/" | sed "s/Jun/06/" | sed "s/Sep/09/" | sed "s/Dec/12/" | sed "s///g" >> data/quarterly.csv 
 
