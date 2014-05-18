3663b3144ebca6695a3a7b985c77841d08b285c2
Rufus Pollock <rufus.pollock@okfn.org>
2014-05-18 19:31:52 +0100

    [annual][s]: store proper dates rather than just year (1984 -> 1984-12-31).

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Year,Rate" > annual.csv
-cat cache/10y.csv | tail -n+2 | sed "s/^.\{7\}//" | sed "s///g" >> annual.csv
+cat cache/10y.csv | tail -n+2 | sed "s/^.\{7\}\(....\)/\1-12-31/" | sed "s///g" >> annual.csv
